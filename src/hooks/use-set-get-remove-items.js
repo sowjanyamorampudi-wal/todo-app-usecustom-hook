7ec7c0be0e5c3d39a085c0be5d1e516271b78dcf
sowjanya <sowjanya.morampudi@westaglielabs.com>
2022-02-10 10:31:49 +0530

    made changes

--- src/hooks/use-set-get-remove-items.js
+++ src/hooks/use-set-get-remove-items.js
@@ -1,28 +1,36 @@
 import { useState, useEffect, useCallback } from "react";
 
 const useLocalStorage = (key, defaultValue) => {
-    const [value, setValue] = useState(() => {
-        let currentValue;
-        try {
-            currentValue = JSON.parse(
-                localStorage.getItem(key) || String(defaultValue)
-            );
-        } catch (error) {
-            currentValue = defaultValue;
-        }
-        return currentValue;
-    });
-    useEffect(() => {
+  const [value, setValue] = useState(() => {
+    let currentValue;
+    try {
+      currentValue = JSON.parse(
+        localStorage.getItem(key) || String(defaultValue)
+      );
+    } catch (error) {
+      currentValue = defaultValue;
+    }
+    return currentValue;
+  });
+  useEffect(() => {
+    if (value) {
+      try {
         localStorage.setItem(key, JSON.stringify(value));
-    }, [value, key]);
-    // const removeItemFunc = () => {
-    //     localStorage.removeItem(key);
-    // };
-    const removeItemFun = useCallback(
-        (key) => {
-            localStorage.removeItem(key);
-        }, [key]
-    );
-    return [value, setValue, removeItemFun];
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, [value, key]);
+
+  // const removeItemFunc = () => {
+  //     localStorage.removeItem(key);
+  // };
+  const removeItemFun = useCallback(
+    (key) => {
+      localStorage.removeItem(key);
+    },
+    [key]
+  );
+  return [value, setValue, removeItemFun];
 };
 export default useLocalStorage;
